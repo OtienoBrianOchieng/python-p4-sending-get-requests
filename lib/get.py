b9135a3b3b29d06d459c2e5e6f7891b3c35f55cb
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-09 13:21:53 +0300

    complete lab

--- lib/get.py
+++ lib/get.py
@@ -8,3 +8,9 @@ response = requests.get(url)
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+print ('******************************')
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+response = requests.get(url)
+print(response.text)
